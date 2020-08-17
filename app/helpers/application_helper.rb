da3633a4d84cacfd2c5f7346075ad5577d30c3be
SNMellott <snmellott@gmail.com>
2020-08-16 20:34:34 -0400

    Finished up general layout things. Moving on to login stuff.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   #Returns the full title on a per page basis
   def full_title(page_title = '')
     base_title = "Organizer Application"
-    if page_title.empty?
+    if page_title.blank?
       base_title
     else
       page_title + " | " + base_title
