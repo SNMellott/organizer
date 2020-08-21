b3ab05f91b5d0f3f0d29eed26b6da2a316de7eb2
SNMellott <snmellott@gmail.com>
2020-08-21 07:37:51 -0400

    Added a user index and admin users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_180420) do
+ActiveRecord::Schema.define(version: 2020_08_21_112942) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_180420) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
