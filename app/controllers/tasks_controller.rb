a1d163c0bcc3618aa01e03746f98cf8c819cbd6a
SNMellott <snmellott@gmail.com>
2020-08-21 09:08:03 -0400

    Added a form to input tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,22 @@
 class TasksController < ApplicationController
+  before_action :logged_in_user, only [:create, :destroy]
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "Task Posted!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :information)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tasks, dependent: [:destroy]
+  has_many :tasks
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
