542377dc2daf0ee6545438a0730b02c25aca09e9
SNMellott <snmellott@gmail.com>
2020-09-17 11:33:43 -0400

    Getting Task Type set up

---
+++ app/controllers/task_type_controller.rb
@@ -0,0 +1,2 @@
+class TaskTypeController < ApplicationController
+end
---
+++ test/controllers/task_type_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TaskTypeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
